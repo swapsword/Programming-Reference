7e80618de07c306f1d2febe3e72a89990153be84
Swapnil Pathak <swapsword@users.noreply.github.com>
2017-05-31 15:05:19 +0200

    Create checkValuePresentInsideArray.js

---
+++ checkValuePresentInsideArray.js
@@ -0,0 +1,6 @@
+// before is the code for checking whether value is present inside the array if present return true
+if (pleaseEnterYourArrayHere.indexOf(pleaseEnterValueHereWhichYouWantToCheckInsideArray) === -1) { 
+return true; 
+} else { 
+return false;
+}
